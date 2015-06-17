7328b0100c6bf1d7a660764f311b623919484098
Tomáš Beseda <mail@tomasbeseda.cz>
2015-06-17 10:54:31 +0200

    Added database connection

--- server/server.js
+++ server/server.js
@@ -6,68 +6,21 @@ if (Meteor.isServer) {
     var time = moment().format('H:mm');
     var day = moment().format('ddd');
     var gmailSMTP = process.env.MAIL_URL = 'smtp://sender%40gmail.com:password@smtp.gmail.com:465/';
-    //console.log(time);
+    var taskFinder = Clients.find().fetch();
 
-    if (time === "7:00"){
-      gmailSMTP;
+    for(var i = 0; i < taskFinder.length; ++i){
+      if(taskFinder[i].time == time){
+        gmailSMTP;
         Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Breakfast',
-          html: Handlebars.templates['example']({ name: 'client name' })
+          to: taskFinder[i].email,
+          from: 'Coach',
+          subject: taskFinder[i].subject,
+          html: taskFinder[i].body
         });
-      console.log("Breakfast sent");
+        console.log(taskFinder[i].subject + " sent to " + taskFinder[i].email + " at " + time);
+      }
     }
-    else if (time === "7:15" && day === "Thu"){
-      gmailSMTP;
-        Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Cardio',
-          html: Handlebars.templates['example']({ name: 'client name' })
-        });
-      console.log("Cardio sent");
-    } 
-    else if (time === "7:30"){
-      gmailSMTP;
-        Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Workout',
-          html: Handlebars.templates['example']({ name: 'client name' })
-        });
-      console.log("Workout sent");
-    }
-    else if (time === "11:00"){
-      gmailSMTP;
-        Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Lunch',
-          html: Handlebars.templates['example']({ name: 'client name' })
-        });
-      console.log("Lunch sent");
-    }
-    else if (time === "14:30"){
-      gmailSMTP;
-        Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Snack',
-          html: Handlebars.templates['example']({ name: 'client name' })
-        });
-      console.log("Snack sent");
-    }
-    else if (time === "19:00"){
-      gmailSMTP;
-        Email.send({
-          to: 'recipient@example.com',
-          from: 'from',
-          subject: 'Dinner',
-          html: Handlebars.templates['example']({ name: 'client name' })
-        });
-      console.log("Dinner sent");
-    }
+    
   }
 
   var cron = new Meteor.Cron( {
